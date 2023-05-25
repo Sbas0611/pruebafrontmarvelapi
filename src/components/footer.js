6c6cd8af8881b36f170a067efd5ab2d00e36a86f
Sbas0611 <dev.arboleda@gmail.com>
2023-05-25 15:49:45 -0500

    change link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Link from 'next/link';
 import { footer } from "flowbite-react";
+
 
 export default function Footer() {
   return (
     
 <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://www.linkedin.com/in/sebasti%C3%A1n-arboleda/" class="hover:underline">Arboleda™</a>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="https://www.linkedin.com/in/sebasti%C3%A1n-arboleda/" class="hover:underline">Arboleda™</Link>. All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
